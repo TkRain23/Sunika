bdcb1b8882f52d61ab49de33439afa0310c7a3e4
tkrain23 <tk.rain09@gmail.com>
2019-01-12 18:40:32 -0800

    updating routes to encrypt passwords, login, and logoout

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = (app) => {
     // sign up get
@@ -8,21 +9,60 @@ module.exports = (app) => {
 
     // sign up post
     app.post('/sign-up', (req,res) => {
-        // create user
+        // create user and jwt
         const user = new User(req.body);
 
         user
             .save()
             .then(user => {
+                var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+                res.cookie('nToken', token, { maxAge: 900000 , httpOnly: true });
                 res.redirect('/collection');
             })
             .catch(err => {
                 console.log(err.message);
+                return res.status(400).send({ err: err });
             });
     });
 
     app.get('/login', (req, res) => {
         res.render('login');
     })
+
+    app.post('/login', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // find this username
+        User.findOne({ username }, 'username password')
+            .then(user => {
+                if (!user) {
+                    // user not found
+                    return res.status(401).send({ message: 'Wrong Username or Password' });
+                }
+
+                // check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        // password does not match
+                        return res.status(401).send({ message: 'Wrong Username or Password' });
+                    }
+                    // create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                        expiresIn: '60 days'
+                    });
+                    // set a cookie and redirect to root
+                    res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                    res.redirect('/');
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    })
+
+    app.get('/logout', (req, res) => {
+        res.clearCookie('nToken');
+        res.redirect('/');
+    });
 
 };
