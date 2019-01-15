a78d5c28045b273d72826ef181df1d5e72237e4b
tkrain23 <tk.rain09@gmail.com>
2019-01-14 22:03:21 -0800

    establishing routes for profile and adding shoes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user')
+const Shoe = require('../models/shoes')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', function(req, res, next) {
+    var currentUser = req.user;
+    const username = req.params.username;
+    User.findOne({username}).populate('shoecollection').then((user) => {
+        res.render('collection', {currentUser})
+    })
+});
+
+router.post('/add/:shoeid', function(req, res) {
+    const username = req.user.username;
+    const shoeId = req.params.shoe._id;
+    User.findOne({username}).then((user) => {
+        user.shoecollection.unshift(shoeId);
+        user.save();
+        res.redirect('/' + username + '/collection')
+    })
 });
 
 module.exports = router;
