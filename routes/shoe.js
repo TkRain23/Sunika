741f7e5eff3c746ba29de4ee8c14c9348fa5e171
tkrain23 <tk.rain09@gmail.com>
2019-01-14 22:05:55 -0800

    adding route for shoes

---
+++ routes/shoe.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+var User = require("../models/user");
+const Shoe = require('../models/shoes')
+
+router.get('/:shoe', (req, res) => {
+    var currentUser = req.user;
+    Shoe
+        .findOne({ urlKey: req.params.shoe })
+        .then(shoe => res.render('single-shoe', {shoe, currentUser}))
+        .catch(err => console.error(err))
+})
+
+module.exports = router;
